446a3ec23a5850493505c594363636373beb3545
pranathi maddineni <pranathi@sixforces.com>
2023-04-18 12:55:25 +0530

    fix

--- src/Components/Tracking/TrackingMap.js
+++ src/Components/Tracking/TrackingMap.js
@@ -1,8 +1,5 @@
 import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
-//const SOCKET_SERVER_URL = 'https://prod.swiftbel.com';
-import WebSocket from 'ws';
-
 const socket = io('https://prod.swiftbel.com');
 
 function TrackingMap() {
@@ -10,54 +7,6 @@ function TrackingMap() {
   const [
     //directions,
     setDirections] = useState(null);
- // const [socket, setSocket] = useState(null);
-
-  // useEffect(() => {
-  //   const newSocket = io(SOCKET_SERVER_URL,{
-  //     cors: {
-  //       origin: 'http://localhost:3000',
-  //       methods: ['GET', 'POST'],
-  //       allowedHeaders: ['my-custom-header'],
-  //       credentials: true
-  //     }
-  //   });
-  //   newSocket.on('connect', () => {
-  //     console.log('Socket connected');
-  //   });
-  //   newSocket.on('error', (error) => {
-  //     console.error('Socket error:', error);
-  //   });
-  //   newSocket.on('connect_error', (error) => {
-  //     console.error('Socket connection error:', error);
-  //   });
-  //   setSocket(newSocket);
-  //   return () => {
-  //     newSocket.close();
-  //   };
-  // }, []);
-  // console.log(socket,'locationn')
-
-    // useEffect(() => {
-    //   const socket = io('https://prod.swiftbel.com');
-    //   socket.on('connection', () => {
-    //     console.log('Connected to Socket.io server');
-    //   });
-
-    //   socket.on('join_room', (data) => {
-    //     console.log('Received conversation:', data);
-    //   });
-
-    //   return () => {
-    //     socket.disconnect();
-    //   };
-    // }, []);
-
-    // const handleClick = () => {
-    //   const socket = io('https://prod.swiftbel.com');
-    //   socket.emit('send_message', { text: 'Hello, server!' });
-    //   socket.disconnect();
-    // };
-
     useEffect(() => {
       socket.on("join_room", () => {
         console.log('connected');
@@ -67,13 +16,6 @@ function TrackingMap() {
         socket.disconnect();
       };
     }, [socket]);
-
-    // const sendMessage = () => {
-    //   socket.emit('send_message', {
-    //     room: 'join_room',
-    //     message: 'Hello, server!'
-    //   });
-    // };
 
   useEffect(() => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
@@ -136,71 +78,3 @@ function TrackingMap() {
 
 export default TrackingMap;
 
-
-
-// import { useState, useEffect } from 'react';
-
-// function TrackingMap() {
-//   const [map, setMap] = useState(null);
-//   const [directions, setDirections] = useState(null);
-//   const [driverMarker, setDriverMarker] = useState(null);
-
-//   useEffect(() => {
-//     const map = new window.google.maps.Map(document.getElementById('map'), {
-//       center: { lat: 37.7749, lng: -122.4194 },
-//       zoom: 8,
-//     });
-
-//     setMap(map);
-//   }, []);
-
-//   useEffect(() => {
-//     if (map) {
-//       const directionsService = new window.google.maps.DirectionsService();
-//       const directionsRenderer = new window.google.maps.DirectionsRenderer({
-//         map,
-//       });
-
-//       const destination = new window.google.maps.LatLng(37.8716, -122.2727);
-
-//       // Get the user's current location
-//       navigator.geolocation.getCurrentPosition((position) => {
-//         const { latitude, longitude } = position.coords;
-//         const origin = new window.google.maps.LatLng(latitude, longitude);
-
-//         const request = {
-//           origin,
-//           destination,
-//           travelMode: window.google.maps.TravelMode.DRIVING,
-//         };
-//         directionsService.route(request, (result, status) => {
-//           if (status === window.google.maps.DirectionsStatus.OK) {
-//             directionsRenderer.setDirections(result);
-//             setDirections(result);
-
-//             const driverMarker = new window.google.maps.Marker({
-//               position: origin,
-//               map,
-//               icon: {
-//                 url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-//               },
-//             });
-
-//             setDriverMarker(driverMarker);
-
-//             // Watch for changes in the driver's location
-//             navigator.geolocation.watchPosition((position) => {
-//               const { latitude, longitude } = position.coords;
-//               const newLocation = new window.google.maps.LatLng(latitude, longitude);
-//               driverMarker.setPosition(newLocation);
-//             });
-//           }
-//         });
-//       });
-//     }
-//   }, [map]);
-
-//   return <div id="map" style={{ height: '100vh' }} />;
-// }
-
-// export default TrackingMap;
